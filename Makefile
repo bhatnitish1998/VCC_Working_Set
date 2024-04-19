ea57822d3d722465ee5651e72158eee2b6fd5ef3
bhatnitish1998 <bhatnitish1998@gmail.com>
2024-04-19 21:13:59 +0530

    added random memory access to guest

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ CFLAGS = -Wall -Wextra -Werror -O2 -Werror=array-bounds=0
 
 all: guest.bin simple_kvm
 
-simple_kvm: simple_kvm.c
-	gcc -o $@ $^
+simple_kvm: simple_kvm.c simple_kvm.h
+	gcc -o $@ $<
 
 guest.bin: guest.o
 	ld -m elf_i386 --oformat binary -e main -Ttext 0 -o $@ $^
--- guest.c
+++ guest.c
@@ -1,5 +1,33 @@
 #include <stdint.h>
 
+long result_location = 0x1600000; // 25 MB
+long begin = 50 *1024*1024 ; // 50 MB
+uint32_t mem_access_size_addr = 0xA00000;
+uint32_t mem_random_addr = 0xA00008; // Percentage of memory access that should be random
+uint32_t overflow_counter_addr = 0xA00010; // To track performance of guest
+uint32_t randomness_addr = 0xA00018; // 0 for contiguous 1 for random access.
+
+const long long a = 1103515245;
+const long long c = 12345;
+const long long m = 2147483648;
+long long seed =123456789;
+
+long lcg_rand() {
+    long  rand = (a * seed + c) %m;
+    seed = rand;
+    return rand;
+}
+
+int increment_counter(int value)
+{
+    value++;
+    if(value ==0)
+    {
+        long k = *(long*) overflow_counter_addr;
+        *(long*) overflow_counter_addr =k+1;
+    }
+    return value;
+}
 
 void hc_print_32_bit(uint32_t val) {
     asm("outl %0,%1" : /* empty */ : "a" (val), "Nd" (0xE1) : "memory");
@@ -13,15 +41,46 @@ uint32_t hc_read_32_bit() {
 
 int main() {
 
-    long begin = 0x3200000; // 50 MB
-    long end = 0x6400000; // 100 MB
+    *(long*) overflow_counter_addr =0;
+    int k =0;
 
     while(1)
     {
-        for(long i=begin;i<=end;i+=0x1000)
+        volatile long mem_pages = (*((long*) mem_access_size_addr))*1024 /4;
+        volatile long rand_percent = *(long*) mem_random_addr;
+
+        long random_pages;
+        long contiguous_pages;
+        if(rand_percent !=0)
         {
-            *(long *) (+(i)) = 42;
+            random_pages = (mem_pages * rand_percent)/100;
+            contiguous_pages = mem_pages - random_pages;
         }
+        else
+        {
+            random_pages =0;
+            contiguous_pages =mem_pages;
+        }
+
+        long x;
+        long sum =0;
+
+        seed =123456789;
+        for( int i =0; i< random_pages; i++)
+        {
+            long addr = lcg_rand() % 0x20000;
+            x = *(long *)(addr*0x1000);
+            sum += x;
+            increment_counter(k);
+        }
+
+        for( int j =0; j< contiguous_pages; j++) {
+            x = *(long *) (begin + (j * 0x1000));
+            sum+=x;
+            increment_counter(k);
+        }
+
+        *(long *) result_location = sum;
     }
     return 0;
 }
