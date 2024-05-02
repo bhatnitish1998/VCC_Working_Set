44dbfd73db83d0392b5d6f232b0dc079e3c6dce1
bhatnitish1998 <bhatnitish1998@gmail.com>
2024-05-03 01:58:33 +0530

    added python script for accuracy testing

--- generate_graphs.py
+++ generate_graphs.py
@@ -1,3 +1,5 @@
+import bisect
+
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -55,6 +57,30 @@ def read_wss_estimate(filename):
 
     return time, wss
 
+
+def get_error(x,y,x1,y1):
+
+    x_values = [i for i in x]
+    x_values.extend(x1)
+    x_values.sort()
+    y_actual =[]
+    for val in x_values:
+        index = bisect.bisect_right(x,val)-1
+        y_actual.append(y[index])
+
+    y_pred =[]
+    for val in x_values:
+        index = bisect.bisect_right(x1,val)-1
+        y_pred.append(y1[index])
+
+    error =0
+    for i in range(len(y_pred)):
+        if y_actual[i] != 0:
+            error += abs(y_pred[i]-y_actual[i])/y_actual[i]
+        else:
+            error += abs(y_pred[i]-y_actual[i])
+
+    return error/100
 
 # Generate graphs
 if not os.path.exists('graphs'):
@@ -144,4 +170,136 @@ plt.savefig('graphs/performance_rate.png')
 plt.clf()
 
 
+################# Accuracy graphs
 
+
+# Plot accuracy graph with respect to sample size
+
+# 0% randomness
+x0,y0 = read_workload("workload_normal.txt")
+x1,y1 = read_wss_estimate("logs/r_0_100.txt")
+x2,y2 = read_wss_estimate("logs/r_0_1000.txt")
+x3,y3 = read_wss_estimate("logs/r_0_10000.txt")
+
+e1=get_error(x0,y0,x1,y1)
+e2=get_error(x0,y0,x2,y2)
+e3=get_error(x0,y0,x3,y3)
+
+plt.step(x0,y0,label ="Actual")
+plt.step(x1, y1,label="sample size = 100 pages")
+plt.step(x2, y2,label="sample size = 1000 pages")
+plt.step(x3, y3,label="sample size = 10000 pages")
+plt.legend()
+plt.title('Memory randomness 0%')
+plt.xlabel('Time')
+plt.ylabel('Memory Usage (MB)')
+plt.grid(True)
+
+plt.text(0, 250, f"Error rate\n100 pg = {e1:.2f}%\n1000 pg  = {e2:.2f}\n10000 pg = {e3:.2f}%", fontsize=12)
+
+plt.savefig('graphs/accuracy_size_r0.png')
+plt.clf()
+
+# 20% randomness
+x0,y0 = read_workload("workload_normal.txt")
+x1,y1 = read_wss_estimate("logs/r_20_100.txt")
+x2,y2 = read_wss_estimate("logs/r_20_1000.txt")
+x3,y3 = read_wss_estimate("logs/r_20_10000.txt")
+
+e1=get_error(x0,y0,x1,y1)
+e2=get_error(x0,y0,x2,y2)
+e3=get_error(x0,y0,x3,y3)
+
+plt.step(x0,y0,label ="Actual")
+plt.step(x1, y1,label="sample size = 100 pages")
+plt.step(x2, y2,label="sample size = 1000 pages")
+plt.step(x3, y3,label="sample size = 10000 pages")
+plt.legend()
+plt.title('Memory randomness 20%')
+plt.xlabel('Time')
+plt.ylabel('Memory Usage (MB)')
+plt.grid(True)
+
+plt.text(0, 250, f"Error rate\n100 pg = {e1:.2f}%\n1000 pg  = {e2:.2f}\n10000 pg = {e3:.2f}%", fontsize=12)
+
+plt.savefig('graphs/accuracy_size_r20.png')
+plt.clf()
+
+# 100% randomness
+x0,y0 = read_workload("workload_normal.txt")
+x1,y1 = read_wss_estimate("logs/r_100_100.txt")
+x2,y2 = read_wss_estimate("logs/r_100_1000.txt")
+x3,y3 = read_wss_estimate("logs/r_100_10000.txt")
+
+e1=get_error(x0,y0,x1,y1)
+e2=get_error(x0,y0,x2,y2)
+e3=get_error(x0,y0,x3,y3)
+
+plt.step(x0,y0,label ="Actual")
+plt.step(x1, y1,label="sample size = 100 pages")
+plt.step(x2, y2,label="sample size = 1000 pages")
+plt.step(x3, y3,label="sample size = 10000 pages")
+plt.legend()
+plt.title('Memory randomness 100%')
+plt.xlabel('Time')
+plt.ylabel('Memory Usage (MB)')
+plt.grid(True)
+
+plt.text(0, 250, f"Error rate\n100 pg = {e1:.2f}%\n1000 pg  = {e2:.2f}\n10000 pg = {e3:.2f}%", fontsize=12)
+
+plt.savefig('graphs/accuracy_size_r100.png')
+plt.clf()
+
+
+############ Accuracy with sample rate
+
+# normal workload
+x0,y0 = read_workload("workload_normal.txt")
+x1,y1 = read_wss_estimate("logs/im_1_wn.txt")
+x2,y2 = read_wss_estimate("logs/im_2_wn.txt")
+x3,y3 = read_wss_estimate("logs/im_5_wn.txt")
+
+e1=get_error(x0,y0,x1,y1)
+e2=get_error(x0,y0,x2,y2)
+e3=get_error(x0,y0,x3,y3)
+
+plt.step(x0,y0,label ="Actual")
+plt.step(x1, y1,label="sample interval = 1 sec")
+plt.step(x2, y2,label="sample interval = 2 sec")
+plt.step(x3, y3,label="sample interval = 5 sec")
+plt.legend()
+plt.title('Normal Workload')
+plt.xlabel('Time')
+plt.ylabel('Memory Usage (MB)')
+plt.grid(True)
+
+plt.text(0, 250, f"Error rate\n1sec = {e1:.2f}%\n2sec  = {e2:.2f}\n5sec = {e3:.2f}%", fontsize=12)
+
+plt.savefig('graphs/accuracy_rate_normal.png')
+plt.clf()
+
+
+# rapid workload
+x0,y0 = read_workload("workload_rapid.txt")
+x1,y1 = read_wss_estimate("logs/im_1_wr.txt")
+x2,y2 = read_wss_estimate("logs/im_2_wr.txt")
+x3,y3 = read_wss_estimate("logs/im_5_wr.txt")
+
+e1=get_error(x0,y0,x1,y1)
+e2=get_error(x0,y0,x2,y2)
+e3=get_error(x0,y0,x3,y3)
+
+plt.step(x0,y0,label ="Actual")
+plt.step(x1, y1,label="sample interval = 1 sec")
+plt.step(x2, y2,label="sample interval = 2 sec")
+plt.step(x3, y3,label="sample interval = 5 sec")
+plt.legend()
+plt.title('Rapid Workload')
+plt.xlabel('Time')
+plt.ylabel('Memory Usage (MB)')
+plt.grid(True)
+
+plt.text(1, 0, f"Error rate\n1sec = {e1:.2f}%\n2sec  = {e2:.2f}\n5sec = {e3:.2f}%", fontsize=12)
+
+plt.savefig('graphs/accuracy_rate_rapid.png')
+plt.clf()
